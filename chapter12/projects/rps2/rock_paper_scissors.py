f6a28b2787ca4b76b78be01a2afaa0988f04d928
Chris Redmond <redmond50@gmail.com>
2025-04-21 21:10:22 -0400

    ch12 R-P-S

--- chapter12/projects/rps2/rock_paper_scissors.py
+++ chapter12/projects/rps2/rock_paper_scissors.py
@@ -12,3 +12,42 @@ def _user_hand() -> str:
             return u[0].lower()
         except (EOFError, KeyboardInterrupt):
             print("\nThanks for playing the game!")
+            sys.exit()
+        except: 
+            print(f"Invalid input, {u}. Try again")
+            continue
+
+def _computer_hand() -> str:
+    return random.choice((_R, _P, _S))
+
+
+
+def _determine_win(u: str, c: str) -> str:
+    if c == u:
+        return TIE
+    if u == _R and c == _S or u == _P and c == _R or u == _S and c == _P:
+        return WIN
+    return LOSS
+
+
+def play_a_round() -> str: 
+    u = _user_hand()
+    c = _computer_hand()
+    print(f"You -- {_to_str(u)} vs {_to_str(c)} -- Computer")
+
+    return _determine_win(u, c)
+
+
+def _to_str(h: str) -> str:
+    if h == _R: 
+        return "Rock"
+    elif h == _P:
+        return "Paper"
+    elif h == _S:
+        return "Scissors"
+    else:
+        return ""
+
+
+if __name__ == "__main__":
+    play_a_round()
