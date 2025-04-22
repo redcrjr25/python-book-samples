4fc63ef3c871271ae0cd89150c580d192d2fbd6b
Chris Redmond <redmond50@gmail.com>
2025-04-22 16:43:44 -0400

    ch 13 changes

---
+++ chapter13/13.8rps3.py
@@ -0,0 +1,19 @@
+from rock_paper_scissors import user_hand, computer_hand, determine_win, WIN
+from output import print_result
+
+def f2(max_rounds: int = 3) -> None:
+    """Plays a fixed number of rounds and prints total wins and rounds."""
+    wins = 0
+    for i in range(max_rounds):
+        user = user_hand()
+        computer = computer_hand()
+        result = determine_win(user, computer)
+        print_result(user, computer, result)
+        if result == WIN:
+            wins += 1
+    print(f"You won {wins} out of {max_rounds} rounds.")
+
+    if __name__ == "__main__":
+        f2(3)
+
+    
---
+++ chapter13/output.py
@@ -0,0 +1,22 @@
+def horz_bar():
+    print("+" * 4 + "-" * 35 + "+" * 4)
+
+def start_banner():
+    horz_bar()
+    print("Let's play Rock Paper Scissors!")
+    horz_bar()
+
+def end_banner(wins: int = 0, rounds: int = 0):
+    print("Thanks for playing Rock Paper Scissors!")
+    if rounds > 0:
+        print(f">>> You won {wins} rounds out of {rounds}. <<<")
+    horz_bar()
+
+def print_result(wlt: str):
+    """Print a text corresponding to win/loss/tie."""
+    if wlt == "w":
+        print("You win!")
+    elif wlt == "l":
+        print("You lose!")
+    else:
+        print("Tie!")
